2cec16fd5abaa11d8bf2e8b0f22522bed118759c
petercz1 <email.peter.carroll@gmail.com>
2020-01-15 11:23:06 +0800

    So you're back... about time...

--- js/map/map-main.js
+++ js/map/map-main.js
@@ -22,6 +22,7 @@ class MapMain {
   // contaxts backend 
   async contact_backend(criteria) {
     let info = document.getElementById('info');
+    info.inner
     window.addresses = await new BackEnd(criteria);
     if (addresses.error) {
       return err.innerHTML = addresses.error + ': ' + addresses.error_no;
